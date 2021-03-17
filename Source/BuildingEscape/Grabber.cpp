bdb4ced396f8b30c492996fcfd2e4f7e8036e898
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-03-17 12:54:13 -0700

    RayCasting and InputBinding
    -Set up the grabber file to ray cast to objects in a certain range
    -Set up and bound player input to a grab function that will be implemented

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -22,9 +22,25 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting For Duty!"));
 	
+	//Check for Physics Handle Component
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle != nullptr){
+		//Physics handle is found
+	}else{
+		UE_LOG(LogTemp, Error, TEXT("%s does not have the Physics Handle Component!"), *GetOwner()->GetName());
+	}
+
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if(InputComponent != nullptr){
+		UE_LOG(LogTemp, Warning, TEXT("Input Component has been found!"));
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	}
+	
+}
+
+void UGrabber::Grab(){
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Pressed"));
 }
 
 
@@ -59,9 +75,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		5.0f
 	);
 	
-
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
 	// Ray-cast out to a certain distance (Reach)
-
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+	
+	AActor* ActorHit = Hit.GetActor();
+	//Log out to test
+	if(Hit.Actor != nullptr){
+		UE_LOG(LogTemp, Warning, TEXT("The object hit by the line was: %s"), *(ActorHit->GetName()));
+	}
+	
 	//See what the ray hits
 }
 
